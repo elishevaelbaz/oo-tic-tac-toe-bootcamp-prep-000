1becd878bb156cb099a4d911f059b8d4bb1b6d6a
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-05 16:30:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,14 +66,14 @@ class TicTacToe
   
   
   def won?
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-      
-    position_1 = @board[win_index_1]
-    position_2 = @board[win_index_2]
-    position_3 = @board[win_index_3]
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+        
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
       
       if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
@@ -82,8 +82,8 @@ class TicTacToe
     return false
   end
   
-  def full?(board)
-    board.all? { |element| element != "" && element != " "}
+  def full?
+    @board.all? { |element| element != "" && element != " "}
   end
 
 end
