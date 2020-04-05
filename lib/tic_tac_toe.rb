24bdaeeb892ceccfee423d275012e9d24af2a567
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-05 16:24:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,10 @@ class TicTacToe
   def move(index, current_player)
     @board[index] = current_player
   end
+  
+  def position_taken?(index)
+    @board[index] != " " && @board[index] != ""
+  end
 
 end
 
