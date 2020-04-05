4982bce8f806398821add69700b54419518acf27
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-05 16:25:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,6 +37,28 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end 
+  
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+  
+  def turn_count(board)
+    counter = 0
+    board.each do |space|
+      if space == "X" || space =="O"
+        counter+=1
+      end
+    end
+    counter
+  end
 
 end
 
