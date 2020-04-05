fd61f92d0008eaede9e7212077d8a85e2dab9152
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-05 16:29:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,12 +75,12 @@ class TicTacToe
     position_2 = @board[win_index_2]
     position_3 = @board[win_index_3]
       
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      end
     end
+    return false
   end
-  return false
-end
 
 end
 
